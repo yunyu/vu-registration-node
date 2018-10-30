ff2649ba2fbcdd1c566675e2861aaaab3f3e462c
Sameer <11097096+sameer@users.noreply.github.com>
2018-10-30 10:51:26 -0500

    Remove tough-cookie-file-store to fix cookie saving and rename YES endpoints to fix bad redirects

--- vu-registration.js
+++ vu-registration.js
@@ -1,143 +1,169 @@
-const fs = require('fs-extra');
-const querystring = require('querystring');
-const cheerio = require('cheerio');
-const FileCookieStore = require('tough-cookie-file-store');
-const jsonic = require('jsonic');
-const sleep = require('sleep-promise');
+const fs = require("fs-extra");
+const querystring = require("querystring");
+const cheerio = require("cheerio");
+const tough = require("tough-cookie");
+const jsonic = require("jsonic");
+const sleep = require("sleep-promise");
 
-const cookieJarPath = 'cookies.json';
-const dataPath = 'data.json';
+const cookieJarPath = "cookies.json";
+const dataPath = "data.json";
 
-let rp = require('request-promise-native');
+let rp = require("request-promise-native");
 
 function printUsage() {
-    console.log(
-        'Usage:\n' +
-            'node vu-registration.js savecookies\n' +
-            'node vu-registration.js register 0420:true,0069:false\n\n' +
-            '* Username and password should be set via the VUNET_ID and VUNET_PW environment variables\n' +
-            '  (use your VUnet ID, NOT your commodore ID)\n' +
-            '* You can only register for courses that are already in your cart\n' +
-            '* The course ID is in the top left corner of the course description dialog\n' +
-            '* The boolean parameter in the course list is equivalent to "Waitlist If Full"'
-    );
+  console.log(
+    "Usage:\n" +
+      "node vu-registration.js savecookies\n" +
+      "node vu-registration.js register 0420:true,0069:false\n\n" +
+      "* Username and password should be set via the VUNET_ID and VUNET_PW environment variables\n" +
+      "  (use your VUnet ID, NOT your commodore ID)\n" +
+      "* You can only register for courses that are already in your cart\n" +
+      "* The course ID is in the top left corner of the course description dialog\n" +
+      '* The boolean parameter in the course list is equivalent to "Waitlist If Full"'
+  );
 }
 
 if (process.argv.length <= 2) {
-    printUsage();
-} else if (process.argv[2] === 'savecookies') {
-    saveCookie(process.env.VUNET_ID, process.env.VUNET_PW)
-        .then(res =>
-            console.log(
-                `Saved cookies for ID ${res.commodoreId} with term code ${
-                    res.termCode
-                }`
-            )
-        )
-        .catch(() =>
-            console.warn('Failed to log in, are your credentials properly set?')
-        );
-} else if (process.argv[2] === 'register' && process.argv.length === 4) {
-    register(jsonic(process.argv[3]))
-        .then(res =>
-            res.enrollmentMessages
-                .map(el => el.detailedMessage)
-                .forEach(el => console.log(el))
-        )
-        .catch(() =>
-            console.warn('Failed to register, are the cookies saved?')
-        );
+  printUsage();
+} else if (process.argv[2] === "savecookies") {
+  saveCookie(process.env.VUNET_ID, process.env.VUNET_PW)
+    .then(res =>
+      console.log(
+        `Saved cookies for ID ${res.commodoreId} with term code ${res.termCode}`
+      )
+    )
+    .catch(e =>
+      console.warn("Failed to log in, are your credentials properly set?" + e)
+    );
+} else if (process.argv[2] === "register" && process.argv.length === 4) {
+  register(jsonic(process.argv[3]))
+    .then(res =>
+      res.enrollmentMessages
+        .map(el => el.detailedMessage)
+        .forEach(el => console.log(el))
+    )
+    .catch(e => console.warn("Failed to register, are the cookies saved?" + e));
 } else {
-    printUsage();
+  printUsage();
 }
 
+const rp_jar = rp.jar();
+
 function initRequestPromise() {
-    const j = rp.jar(new FileCookieStore(cookieJarPath));
+  if (fs.existsSync(cookieJarPath)) {
+    const file_jar = rp.jar();
+    file_jar._jar = tough.CookieJar.deserializeSync(
+      JSON.parse(fs.readFileSync(cookieJarPath))
+    );
+    file_jar.setCookie = function(cookieOrStr, uri, options) {
+      return file_jar._jar.setCookieSync(cookieOrStr, uri, options || {});
+    };
+    file_jar.getCookieString = function(uri) {
+      return file_jar._jar.getCookieStringSync(uri);
+    };
+    file_jar.getCookies = function(uri) {
+      return file_jar._jar.getCookiesSync(uri);
+    };
     rp = rp.defaults({
-        jar: j,
-        headers: {
-            'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0'
-        }
+      jar: file_jar,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0"
+      }
     });
+  } else {
+    rp = rp.defaults({
+      jar: rp_jar,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0"
+      }
+    });
+  }
 }
 
 async function saveCookie(username, password) {
-    for (let path of [cookieJarPath, dataPath]) {
-        if (await fs.exists(path)) {
-            await fs.unlink(path);
-        }
+  for (let path of [cookieJarPath, dataPath]) {
+    if (await fs.exists(path)) {
+      await fs.unlink(path);
     }
-    initRequestPromise();
+  }
+  initRequestPromise();
+  const transform = body => cheerio.load(body);
+  let $ = await rp({
+    uri: "https://acad.app.vanderbilt.edu/student-search/Entry.action",
+    transform
+  });
+  const formData = { action: $("form").attr("action") };
 
-    const transform = body => cheerio.load(body);
-    let $ = await rp({ uri: 'https://yes.vanderbilt.edu', transform });
-    const formData = { action: $('form').attr('action') };
-    const res = await rp.post({
-        uri: `https://sso.vanderbilt.edu${formData.action}`,
-        resolveWithFullResponse: true,
-        followAllRedirects: true,
-        form: {
-            'pf.username': username,
-            'pf.pass': password,
-            'pf.ok': 'clicked'
-        }
-    });
-
-    const commodoreId = querystring.parse(res.request.uri.query).commodoreId;
-    $ = await rp({
-        uri: `https://webapp.mis.vanderbilt.edu/more/SearchClasses!input.action?commodoreIdToLoad=${
-            commodoreId
-        }`,
-        transform
-    });
-    const termCode = $('#selectedTerm')
-        .find('[selected="selected"]')
-        .attr('value');
-
-    if (!termCode || !commodoreId) {
-        throw new Error();
+  const loginRes = await rp.post({
+    uri: `https://sso.vanderbilt.edu${formData.action}`,
+    resolveWithFullResponse: true,
+    followAllRedirects: true,
+    form: {
+      "pf.username": username,
+      "pf.pass": password,
+      "pf.ok": "clicked",
+      "pf.cancel": ""
     }
+  });
 
-    const result = { termCode, commodoreId };
-    await fs.writeFile(dataPath, JSON.stringify(result));
-    return result;
+  const commodoreId = querystring.parse(loginRes.request.uri.query).commodoreId;
+  $ = await rp({
+    uri: `https://acad.app.vanderbilt.edu/more/SearchClasses!input.action?commodoreIdToLoad=${commodoreId}`,
+    transform
+  });
+  const termCode = $("#selectedTerm")
+    .find('[selected="selected"]')
+    .attr("value");
+
+  if (!termCode || !commodoreId) {
+    throw new Error(termCode + " " + commodoreId);
+  }
+
+  const result = { termCode, commodoreId };
+  await fs.writeFile(dataPath, JSON.stringify(result));
+  await fs.writeFile(
+    cookieJarPath,
+    JSON.stringify(rp_jar._jar.serializeSync())
+  );
+  return result;
 }
 
 async function register(courseList) {
-    initRequestPromise();
-    const data = JSON.parse(await fs.readFile(dataPath));
-    let queueEnrollBase = `https://webapp.mis.vanderbilt.edu/more/StudentClass!queueEnroll.action?selectedTermCode=${
-        data.termCode
-    }`;
-    let index = 0;
-    for (const classNumber in courseList) {
-        queueEnrollBase +=
-            '&enrollmentRequestItems%5B' +
-            index +
-            '%5D.classNumber=' +
-            classNumber +
-            '&enrollmentRequestItems%5B' +
-            index +
-            '%5D.waitList=' +
-            courseList[classNumber].toString();
-        ++index;
-    }
+  initRequestPromise();
+  const data = JSON.parse(await fs.readFile(dataPath));
+  let queueEnrollBase = `https://acad.app.vanderbilt.edu/more/StudentClass!queueEnroll.action?selectedTermCode=${
+    data.termCode
+  }`;
+  let index = 0;
+  for (const classNumber in courseList) {
+    queueEnrollBase +=
+      "&enrollmentRequestItems%5B" +
+      index +
+      "%5D.classNumber=" +
+      classNumber +
+      "&enrollmentRequestItems%5B" +
+      index +
+      "%5D.waitList=" +
+      courseList[classNumber].toString();
+    ++index;
+  }
 
-    const transform = body => JSON.parse(body);
-    const queueResult = await rp({ uri: queueEnrollBase, transform });
-    console.log(`Queued with job ID ${queueResult.jobId}`);
+  const transform = body => JSON.parse(body);
+  const queueResult = await rp({ uri: queueEnrollBase, transform });
+  console.log(`Queued with job ID ${queueResult.jobId}`);
 
-    let status = null;
-    await sleep(1000);
-    while (!status || !status.enrollmentMessages) {
-        await sleep(750);
-        status = await rp({
-            uri: `https://webapp.mis.vanderbilt.edu/more/StudentClass!checkStatus.action?jobId=${
-                queueResult.jobId
-            }`,
-            transform
-        });
-    }
-    return status;
+  let status = null;
+  await sleep(1000);
+  while (!status || !status.enrollmentMessages) {
+    await sleep(750);
+    status = await rp({
+      uri: `https://acad.app.vanderbilt.edu/more/StudentClass!checkStatus.action?jobId=${
+        queueResult.jobId
+      }`,
+      transform
+    });
+  }
+  return status;
 }
